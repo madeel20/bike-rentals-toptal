bb9af032930ac60a03027e8c45eaa27229e5c3d9
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2022-09-04 15:01:04 +0500

    update

--- functions/middleware/firebaseAuth.js
+++ functions/middleware/firebaseAuth.js
@@ -1,8 +1,10 @@
 const admin = require('firebase-admin');
 
+/**
+ * @description - Verifies firebase authorization token on each request
+ */
 async function firebaseAuth(req, res, next) {
   try {
-    console.log(req.headers['authorization'])
     req.token = await admin.auth().verifyIdToken(req.headers['authorization']);
     next();
   } catch (err) {
